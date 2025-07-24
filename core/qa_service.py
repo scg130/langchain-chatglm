902b752bd668fb375794cb069918f8d6bf0e40e6
scg130 <scg130@163.com>
2025-07-24 18:22:33 +0800

    81

--- core/qa_service.py
+++ core/qa_service.py
@@ -46,8 +46,8 @@ class QAService:
         try:
             # 准备符合链期望的输入格式
             inputs = {self.input_key: question}
-            # chain = self._get_chain(keywords=question)  # 可以传入关键词过滤
-            chain = self.qa_chain  # 使用初始化时的链
+            chain = self._get_chain(keywords=question)  # 可以传入关键词过滤
+            # chain = self.qa_chain  # 使用初始化时的链
             # 如果链有内存，确保内存系统也能获取到输入
             if hasattr(chain, 'memory') and chain.memory:
                 inputs[self.memory_input_key] = question
