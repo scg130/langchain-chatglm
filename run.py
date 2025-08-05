db0207d3d1e7de2e8dd06f6d8aa6e6593e89ffec
scg130 <scg130@163.com>
2025-08-05 20:15:44 +0800

    210

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8800, reload=True)
+    uvicorn.run(
+        "api.main:app", 
+        host="0.0.0.0", 
+        port=8800, 
+        reload=True
+    )
