e23de86f8a8c683c46832f00d09484101022db84
scg130 <scg130@163.com>
2025-08-27 17:53:24 +0800

    76

--- tools/open-sora.sh
+++ tools/open-sora.sh
@@ -19,7 +19,7 @@ sudo apt autoremove -y
 wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install cuda-12-1
 
 export CUDA_HOME=/usr/local/cuda-12.1
@@ -29,14 +29,27 @@ export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 # create a virtual env and activate (conda as an example)
 conda create -n opensora python=3.10 -y
 conda activate opensora
-conda install -c  conda-forge glibc=2.32 cmake 
+conda install -c  conda-forge cmake  
 
 # download the repo
 git clone https://github.com/hpcaitech/Open-Sora
 cd Open-Sora
 
-sed -i.bak \
-    -e "s/from[[:space:]]\+tensornvme\.async_file_io[[:space:]]\+import[[:space:]]\+\(AsyncFileWriter,\?[[:space:]]*AsyncFileReader\|DiskOffloader\)/from tensornvme import DiskOffloader/" \
+# Ensure torch >= 2.4.0
+pip install -v . 
+pip install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121 
+pip install flash-attn --no-build-isolation --no-cache-dir
+pip install tensornvme
+ 
+# download the model
+pip install "huggingface_hub[cli]"
+huggingface-cli download hpcai-tech/Open-Sora-v2 --local-dir ./ckpts
+
+# 恢复原文件
+# mv opensora/utils/ckpt.py opensora/utils/ckpt.py.bak
+# mv opensora/utils/ckpt.py.bak opensora/utils/ckpt.py
+
+sed  -e "s/from[[:space:]]\+tensornvme\.async_file_io[[:space:]]\+import[[:space:]]\+\(AsyncFileWriter,\?[[:space:]]*AsyncFileReader\|DiskOffloader\)/from tensornvme import DiskOffloader/" \
     -e "s/\(AsyncFileWriter\|AsyncFileReader\)/DiskOffloader/g" \
     opensora/utils/ckpt.py   
 
@@ -52,19 +65,6 @@ except ImportError:\
         return attn @ v' opensora/models/mmdit/math.py
 
 
-# Ensure torch >= 2.4.0
-pip install -v . 
-pip install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121 
-pip install flash-attn --no-build-isolation --no-cache-dir
-pip install tensornvme
-
- 
-# 恢复原文件
-# mv opensora/utils/ckpt.py.bak opensora/utils/ckpt.py
-
- # download the model
-pip install "huggingface_hub[cli]"
-huggingface-cli download hpcai-tech/Open-Sora-v2 --local-dir ./ckpts
 
 # run the inference text-to-video
 torchrun --nproc_per_node 1 --standalone scripts/diffusion/inference.py configs/diffusion/inference/t2i2v_768px.py --save-dir samples --prompt "raining, sea"
