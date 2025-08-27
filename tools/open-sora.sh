821367a81295703aae675f168c422108d20fa308
scg130 <scg130@163.com>
2025-08-27 10:54:10 +0800

    74

--- tools/open-sora.sh
+++ tools/open-sora.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-
 cd /usr/local/src
+
+# cuda 12.1
+sudo apt --purge remove '*cuda*' 'nvidia*'
+sudo apt autoremove -y
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-12-1
+export CUDA_HOME=/usr/local/cuda-12.1
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
 # create a virtual env and activate (conda as an example)
-conda create -n opensora python=3.10
+conda create -n opensora python=3.10 -y
 conda activate opensora
 
 # download the repo
@@ -10,9 +22,9 @@ git clone https://github.com/hpcaitech/Open-Sora
 cd Open-Sora
 
 # Ensure torch >= 2.4.0
-pip install -v . # for development mode, `pip install -v -e .`
-pip install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121 # install xformers according to your cuda version
-pip install flash-attn --no-build-isolation
+pip install -v . 
+pip install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121 
+pip install flash-attn --no-build-isolation --no-cache-dir
 
  # download the model
 pip install "huggingface_hub[cli]"
