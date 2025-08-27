7524318ee1260ef23004d9360dda9a739be519cf
MingDong She <scg130@163.com>
2025-08-28 00:04:10 +0800

    81

--- tools/open-sora.sh
+++ tools/open-sora.sh
@@ -13,19 +13,6 @@ sudo add-apt-repository ppa:axboe/liburing
 sudo apt update -y
 sudo apt install libaio1 build-essential -y
 
-# cuda 12.1
-sudo apt --purge remove '*cuda*' 'nvidia*'
-sudo apt autoremove -y
-wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update -y
-sudo apt-get -y install cuda-12-1
-
-export CUDA_HOME=/usr/local/cuda-12.1
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-
 # create a virtual env and activate (conda as an example)
 conda create -n opensora python=3.10 -y
 conda activate opensora
@@ -54,23 +41,17 @@ sed -i \
   -e 's/from[[:space:]]*tensornvme\.async_file_io[[:space:]]*import[[:space:]]*AsyncFileWriter/from tensornvme import DiskOffloader/' \
   -e 's/\bAsyncFileWriter\b/DiskOffloader/g' \
   opensora/utils/ckpt.py
- 
 
-
-sed -i '/from flash_attn import flash_attn_func as flash_attn_func_v2/c\
-try:\
-    from flash_attn import flash_attn_func as flash_attn_func_v2\
-except ImportError:\
-    print("⚠️ flash-attn not available, falling back to PyTorch attention")\
-    import torch\
-    def flash_attn_func_v2(q, k, v, *args, **kwargs):\
-        attn = torch.softmax(q @ k.transpose(-2, -1) / (q.size(-1) ** 0.5), dim=-1)\
-        return attn @ v' opensora/models/mmdit/math.py
+sudo fallocate -l 32G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 
 
 # run the inference text-to-video
-torchrun --nproc_per_node 1 --standalone scripts/diffusion/inference.py configs/diffusion/inference/t2i2v_768px.py --save-dir samples --prompt "raining, sea"
+torchrun --nproc_per_node 1  --standalone scripts/diffusion/inference.py configs/diffusion/inference/t2i2v_768px.py --save-dir samples --prompt "raining, sea"
 
 # run the inference text-to-video-with-image
 torchrun --nproc_per_node 1 --standalone scripts/diffusion/inference.py configs/diffusion/inference/256px.py --cond_type i2v_head --prompt "A plump pig wallows in a muddy pond on a rustic farm, its pink snout poking out as it snorts contentedly. The camera captures the pig's playful splashes, sending ripples through the water under the midday sun. Wooden fences and a red barn stand in the background, framed by rolling green hills. The pig's muddy coat glistens in the sunlight, showcasing the simple pleasures of its carefree life." --ref assets/texts/i2v.png
