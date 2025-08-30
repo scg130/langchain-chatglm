e2058f1b62fb2c6ecb311cac0806c2aa544f048a
MingDong She <scg130@163.com>
2025-08-31 04:42:19 +0800

    89

--- tools/video/install_videocrafter.sh
+++ tools/video/install_videocrafter.sh
@@ -2,9 +2,11 @@
 cd /usr/local/src
 
 
-conda create -n videocrafter python=3.8.5
+conda create -n videocrafter python=3.8.5 -y
 conda activate videocrafter
 git clone https://github.com/AILab-CVC/VideoCrafter.git
+cd VideoCrafter
+pip install --upgrade "pip<24.1"
 pip install -r requirements.txt
 
 
@@ -14,8 +16,10 @@ pip install huggingface-hub
 # Download the model
 huggingface-cli download --resume-download VideoCrafter/VideoCrafter2 --local-dir checkpoints/base_512_v2 --local-dir-use-symlinks False
 
+pip install gradio==3.41.2
+
 sh scripts/run_text2video.sh
 
-sh scripts/run_image2video.sh
+huggingface-cli download --resume-download VideoCrafter/Image2Video-512 --local-dir checkpoints/i2v_512_v1 --local-dir-use-symlinks False
 
-python gradio_app.py
+sh scripts/run_image2video.sh
