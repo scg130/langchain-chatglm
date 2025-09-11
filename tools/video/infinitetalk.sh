dc075bd62e7d75ce77c7a192593cc8ab30fdbb85
MingDong She <scg130@163.com>
2025-09-12 01:22:55 +0800

    122

--- tools/video/infinitetalk.sh
+++ tools/video/infinitetalk.sh
@@ -20,6 +20,8 @@ conda install -c conda-forge librosa
 
 conda install -c conda-forge ffmpeg
 
+pip install huggingface_hub
+
 huggingface-cli download Wan-AI/Wan2.1-I2V-14B-480P --local-dir ./weights/Wan2.1-I2V-14B-480P
 huggingface-cli download TencentGameMate/chinese-wav2vec2-base --local-dir ./weights/chinese-wav2vec2-base
 huggingface-cli download TencentGameMate/chinese-wav2vec2-base model.safetensors --revision refs/pr/1 --local-dir ./weights/chinese-wav2vec2-base
