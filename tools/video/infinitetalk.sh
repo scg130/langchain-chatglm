2d250cb4e981a2e5fe72f4fac9cb1c28dbd3795a
MingDong She <scg130@163.com>
2025-09-12 01:21:49 +0800

    121

---
+++ tools/video/infinitetalk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cd /usr/local/src
+git clone https://github.com/MeiGen-AI/InfiniteTalk.git
+cd InfiniteTalk
+
+conda create -n multitalk python=3.10
+conda activate multitalk
+pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu121
+pip install -U xformers==0.0.28 --index-url https://download.pytorch.org/whl/cu121
+
+pip install misaki[en]
+pip install ninja 
+pip install psutil 
+pip install packaging
+pip install wheel
+pip install flash_attn==2.7.4.post1
+
+pip install -r requirements.txt
+conda install -c conda-forge librosa
+
+conda install -c conda-forge ffmpeg
+
+huggingface-cli download Wan-AI/Wan2.1-I2V-14B-480P --local-dir ./weights/Wan2.1-I2V-14B-480P
+huggingface-cli download TencentGameMate/chinese-wav2vec2-base --local-dir ./weights/chinese-wav2vec2-base
+huggingface-cli download TencentGameMate/chinese-wav2vec2-base model.safetensors --revision refs/pr/1 --local-dir ./weights/chinese-wav2vec2-base
+huggingface-cli download MeiGen-AI/InfiniteTalk --local-dir ./weights/InfiniteTalk
+
+
+python generate_infinitetalk.py \
+    --ckpt_dir weights/Wan2.1-I2V-14B-480P \
+    --wav2vec_dir 'weights/chinese-wav2vec2-base' \
+    --infinitetalk_dir weights/InfiniteTalk/single/infinitetalk.safetensors \
+    --input_json examples/single_example_image.json \
+    --size infinitetalk-720 \
+    --sample_steps 40 \
+    --mode streaming \
+    --motion_frame 9 \
+    --save_file infinitetalk_res_720p
